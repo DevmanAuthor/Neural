997f1097f3eb2332684171f66952b943a1d5dd0b
DevmanAuthor <main@mail.com>
2018-08-09 16:54:03 +0530

    stuff

--- Game.py
+++ Game.py
@@ -1,13 +1,28 @@
 import pygame
+# from pygame.locals import *
+import Guy
 
 
 size = w, h = 320, 420
 
+pygame.init()
+screen = pygame.display.set_mode(size)
 
-def Init():
-    pygame.init()
-    pygame.display.set_mode(size)
+Dude = Guy.Guy('(def)head.png', '(def)body.png', '(def)(L)arm.png', '(def)(R)arm.png', '(def)legs.png')
 
 
-def Run():
-    print("run! ")
+def main():
+    done = False
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+        screen.blit(Dude.head.gfx, (50, 50))
+        screen.blit(Dude.arm_r.gfx, (50+8, 50+8))
+        screen.blit(Dude.arm_l.gfx, (50-8, 50+8))
+        screen.blit(Dude.body.gfx, (50, 50+8))
+        screen.blit(Dude.legs.gfx, (50, 50+8+8))
+        pygame.display.flip()
+
+
+main()
--- Guy.py
+++ Guy.py
@@ -1,7 +1,20 @@
+import pygame
+import os
+
+
+class BodyPart:
+    def __init__(self, gfx):
+        self.gfx = gfx
+
 
 class Guy:
-    def __init__(self):
+    def __init__(self, head, body, arm_l, arm_r, legs):
         self.emotions = dict(shame=0, fear=0, pain=0, grief=0, joy=0, mood=0)
+        self.head = BodyPart(pygame.image.load(os.path.join('gfx', head)))
+        self.body = BodyPart(pygame.image.load(os.path.join('gfx'), body))
+        self.arm_l = BodyPart(pygame.image.load(os.path.join('gfx'), arm_l))
+        self.arm_r = BodyPart(pygame.image.load(os.path.join('gfx'), arm_r))
+        self.legs = BodyPart(pygame.image.load(os.path.join('gfx'), legs))
 
     def Run(self):
         self.emotions.mood = (self.emotions.griefa / self.emotions.joy)
