84a4a3efa1d272612b8897122a0d1cfbd77fac8d
DevmanAuthor <main@mail.com>
2018-08-11 12:07:09 +0530

    Default_Entity before Changed to an actual Empty Template

--- Game.py
+++ Game.py
@@ -17,11 +17,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
+        '''
         screen.blit(Dude.head.gfx, (50, 50))
         screen.blit(Dude.arm_r.gfx, (50+8, 50+8))
         screen.blit(Dude.arm_l.gfx, (50-8, 50+8))
         screen.blit(Dude.body.gfx, (50, 50+8))
         screen.blit(Dude.legs.gfx, (50, 50+8+8))
+        '''
+        Dude.pos = (50, 50)
+        Dude.Run(screen)
         pygame.display.flip()
 
 
--- Guy.py
+++ Guy.py
@@ -1,20 +1,40 @@
 import pygame
 import os
 
+sn = 20
+
 
 class BodyPart:
-    def __init__(self, gfx):
-        self.gfx = gfx
+    def __init__(self, gfx, pos=(0, 0), sockpoint=(0, 0)):
+        self.pos = pos
+        self.gfx = pygame.image.load(os.path.join('gfx', gfx))
+        self.socket = sockpoint
+
+    def lock_to(self, pos):
+        self.pos = pos
 
 
 class Guy:
-    def __init__(self, head, body, arm_l, arm_r, legs):
-        self.emotions = dict(shame=0, fear=0, pain=0, grief=0, joy=0, mood=0)
-        self.head = BodyPart(pygame.image.load(os.path.join('gfx', head)))
-        self.body = BodyPart(pygame.image.load(os.path.join('gfx'), body))
-        self.arm_l = BodyPart(pygame.image.load(os.path.join('gfx'), arm_l))
-        self.arm_r = BodyPart(pygame.image.load(os.path.join('gfx'), arm_r))
-        self.legs = BodyPart(pygame.image.load(os.path.join('gfx'), legs))
+    def __init__(self, head, body, arm_l, arm_r, legs, pos)
+        self.pos = pos
+        self.head = BodyPart(head)
 
-    def Run(self):
-        self.emotions.mood = (self.emotions.griefa / self.emotions.joy)
+        self.head.socket = (self.head.gfx.get_height()/2, self.head.gfx.get_width()/2)
+        self.head.lock_to(self.pos)
+        
+        self.body = BodyPart(body)
+        self.body.lock_to(self.head.pos)
+
+        self.arm_l = BodyPart(arm_l)
+        self.arm_r = BodyPart(arm_r)
+        self.legs = BodyPart(legs)
+
+    def Run(self, screen):
+        screen.blit(self.head.gfx, self.head.pos)
+        screen.blit(self.body.gfx, self.body.pos)
+        print("\n\n\n Entity base position: ", self.pos)
+        print("Head position: ", self.head.pos)
+        print("Body position: ", self.body.pos)
+
+
+
