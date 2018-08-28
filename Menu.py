38ee9b314cc4fc0ec09cc49a7e61b8e03b48fed7
DevmanAuthor <main@mail.com>
2018-08-28 15:01:57 +0530

    stuff again

--- Menu.py
+++ Menu.py
@@ -1,24 +1,28 @@
 import Tool
 import UI
-import pygbutton
 import pygame
 
-PlayButton = pygbutton.PygButton((100, 100, 0, 0), "Play")
+PlayButton = UI.Button((100, 100, 0, 0), "Play")
 Background = Tool.load_image("gfx/UI/MenuBackground.png")
 
 
 def Load():
     PlayButton.setSurfaces("gfx/UI/BtnElevated.png", "gfx/UI/BtnPressed.png")
 
-    
 
 def Update(event):
-    PlayButton.handleEvent(event)
+    Btnev = PlayButton.handleEvent(event)
+    if 'click' in Btnev:
+        PlayButton.move((10, 10))
+        print(PlayButton.pos)
 
 
 def draw(sheet):
     sheet.blit(Background, (0, 0))
     PlayButton.draw(sheet)
-    pygame.draw.rect(sheet, (225, 0, 0), GoButton._rect)
+    # pygame.draw.rect(sheet, (225, 0, 0), PlayButton._rect)
+    # print(PlayButton.pos, PlayButton._rect)
+
+
 
 
--- UI.py
+++ UI.py
@@ -4,6 +4,8 @@ import Stats
 import Creatures
 import Entity
 import Tool
+import pygbutton
+
 pygame.init()
 
 WHITE = (225, 225, 225)
@@ -28,115 +30,25 @@ class Text():
         self.str = str(string)
 
 
-class Button():
-    def __init__(self, pos, string, font=pygame.font.SysFont(None, 18)):
-        self.pos = pos
-        self.txt = Text(font, string, BLACK)
-        self.gfx = list()
-        self._rect = pygame.Rect(0, 0, 0, 0)
-        self.buttondown = False
-        self.mouseover = False
-        self.lastMouseDownoverButton = False
-        
-    def set_surfaces(self, normal, pressed, hover=0, overlay=0):
-        self.gfx.append(Tool.load_image(normal))
-        self.gfx.append(Tool.load_image(pressed))
-        if hover != 0:
-            self.gfx.append(Tool.load_image(hover))
-        else:
-            self.gfx.append(0)
-        if overlay != 0:
-            self.gfx.append(Tool.load_image(overlay))
-        else:
-            self.gfx.append(0)
-        self._rect = self.gfx[0].get_rect()
-
+class Button(pygbutton.PygButton):
+    def __init__(self,  *args):
+        super(Button, self).__init__(*args)
+        self.pos = (self._rect.x, self._rect.y)
+    
     def move(self, pos):
-        self.pos = pos
+        self._pos = tuple((self._pos[0] + pos[0], self._pos[1] + pos[1]))
+        r = pygame.Rect(self._pos[0], self._pos[1], self._rect[2], self._rect[3])
+        self._propSetRect(r)
     
-    def _get_rect():
-        return pygame.Rect(self.pos[0], self.pos[1], self.gfx[0].get_width(), self.gfx[0].get_height())
-
-    def _update_rect(newrect):
-        self._rect = newrect
-
-    def on_mouseenter(self, event):
-        pass
-
-    def on_mousemotion(self, event):
-        pass
-
-    def on_mousedown(self, event):
-        pass
-
-    def on_mouseexit(self, event):
-        pass
-        
-    def on_mouseclick(self, event):
-        pass
-
-    def handle_event(self, event):
-        if event.type not in (MOUSEMOTION, MOUSEBUTTONUP, MOUSEBUTTONDOWN):
-            return []
-
-        retval = []
-        hasExited = False
-
-        if not self.mouseover and self._rect.collidepoint(event.pos):
-            self.mouseover = True
-            self.on_mouseenter(event)
-            retval.append('enter')
-        elif self.mouseover and not self._rect.collidepoint(event.pos):
-            self.mouseover = False
-            hasExited = True
-        
-        if self._rect.collidepoint(event.pos):
-            if event.type == MOUSEMOTION:
-                self.on_mousemotion(event)
-                retval.append('motion')
-            elif event.type == MOUSEBUTTONDOWN:
-                self.buttondown = True
-                self.lastMouseDownoverButton = True
-                self.on_mousedown(event)
-                retval.append('down')
-        else:
-            if event.type in (MOUSEBUTTONUP, MOUSEBUTTONDOWN):
-                self.lastMouseDownoverButton = False
-        
-        doMouseClick = False
-        if event.type == MOUSEBUTTONUP:
-            if self.lastMouseDownoverButton:
-                doMouseClick = True
-            self.lastMouseDownoverButton = False
-            if self.buttondown:
-                self.buttondown = False
-            if doMouseClick:
-                retval.append('click')
-                self.on_mouseclick(event)
-
-        if hasExited:
-            self.on_mouseexit(event)
-            retval.append('exit')
-        
-        return retval
-
-    def update(self):
-        pass
+    def set_pos(self, pos):
+        self._pos = pos
     
-    def draw(self, sheet):
-        if self.gfx[3] != 0:
-            sheet.blit(self.gfx[3], self.pos)
-        if self.buttondown:
-            sheet.blit(self.gfx[1], self.pos)
-        elif self.mouseover and self.gfx[2] != 0:
-            sheet.blit(self.gfx[2])
-        else:
-            sheet.blit(self.gfx[0], self.pos)
-    
-    rect = property(_get_rect, _update_rect)
+    def get_pos(self):
+        return self._pos
 
+    pos = property(get_pos, set_pos)
+    
 
 text = Text(pygame.font.SysFont(None, 18), Stats.Organic["Health Aura"].value)
-
 
 HUD = {text: 9}
